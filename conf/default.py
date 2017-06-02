7802811fed02d3eb18c4fe25b14a00013852824a
Kam Wlung <kamwlung@gmail.com>
2017-06-02 20:54:33 +0800

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -23,7 +23,7 @@ from django.conf.global_settings import *
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'aa123'
-APP_TOKEN = 'fbc6adb8-1da2-4f65-b6df-30474b373522'
+APP_TOKEN = 'a4182d4e-98b9-437a-b96f-3960edb4b43f'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas.bking.com'
 
