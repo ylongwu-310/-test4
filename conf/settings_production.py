ee6c04a83f9ff5457375adaa60d1e53f5da01b34
Kam Wlung <kamwlung@gmail.com>
2017-08-28 21:03:34 +0800

    Update settings_production.py

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -11,10 +11,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': aa12345,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': wltets,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'bk@321',                        # 你的数据库password
-        'HOST': '10.135.72.32',                   		   # 数据库HOST
+        'HOST': '10.0.1.29',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
