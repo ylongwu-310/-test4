d4ac281da620925b6ee6d358d40cc2543c8e2ec5
Kam Wlung <kamwlung@gmail.com>
2017-08-28 21:02:34 +0800

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -25,7 +25,7 @@ from django.conf.global_settings import *
 APP_ID = 'wltest'
 APP_TOKEN = 'fb64d0f8-9f60-4f25-9f80-5cb5924dfc5b'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = http://paasce-1.hqhui.cn'
+BK_PAAS_HOST = 'http://paasce-1.hqhui.cn'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
