92e46c9ad6003f5f1bfe1b383bb3dedeef9538f9
ylongwu-310 <31396909+ylongwu-310@users.noreply.github.com>
2017-08-29 11:07:39 +0800

    Update views.py

--- home_application/views.py
+++ home_application/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from common.mymako import render_mako_context
+from django.http import HttpResponse
 
+df index(request):
+    return HttpResponse("Hell Word")
 
 def home(request):
     """
