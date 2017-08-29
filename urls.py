351f2d4b87a4d76cafc980ac69b2866b6547d296
ylongwu-310 <31396909+ylongwu-310@users.noreply.github.com>
2017-08-29 11:03:31 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -17,7 +17,8 @@ urlpatterns = patterns(
     # 应用功能开关控制--请勿修改
     url(r'^app_control/', include('app_control.urls')),
     # 在home_application(根应用)里开始开发你的应用的主要功能
-    url(r'^', include('home_application.urls')),
+    #url(r'^', include('home_application.urls')),
+    url(r'^', 'home_application.views.index'),
 )
 
 
