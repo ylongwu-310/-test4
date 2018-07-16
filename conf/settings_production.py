eb44290f2742434eb34d201f1277e259fc2224eb
ylongwu-310 <31396909+ylongwu-310@users.noreply.github.com>
2018-07-16 15:30:35 +0800

    Update settings_production.py

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -11,10 +11,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'bktest',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': '',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'bk@321',                        # 你的数据库password
-        'HOST': '10.0.2.145',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
+        'PASSWORD': '',                        # 你的数据库password
+        'HOST': '',                   		   # 数据库HOST
+        'PORT': '',                        # 默认3306
     },
 }
